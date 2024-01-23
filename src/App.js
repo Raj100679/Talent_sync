dc7d8e7607475a280395c8a32cf64438955be046
Raj <rajkasat@yo.com>
2024-01-23 21:14:13 +0530

    Changed some basic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import Navbar from './Components/Navbar';
-import MidPage from './Components/MidPage';
-import Social from './Components/Social';
-import Advantage from './Components/Advantage';
-import Testimonial from './Components/Testimonial';
-import Faq from './Components/Faq';
-import Trial from './Components/Trial';
-import Footer from './Components/Footer';
-import LastFooter from './Components/LastFooter';
-
+import Navbar from "./Components/Navbar";
+import MidPage from "./Components/MidPage";
+import Social from "./Components/Social";
+import Advantage from "./Components/Advantage";
+import Testimonial from "./Components/Testimonial";
+import Faq from "./Components/Faq";
+import Trial from "./Components/Trial";
+import Footer from "./Components/Footer";
+import LastFooter from "./Components/LastFooter";
+// import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <MidPage/>
-      <Social/>
-      <Advantage/>
-      <Testimonial/>
-      <Faq/>
-      <Trial/>
-      <Footer/>
-      <LastFooter/>
+      <Navbar />
+      <MidPage />
+      <Social />
+      <Advantage />
+      <Testimonial />
+      <Faq />
+      <Trial />
+      <Footer />
+      <LastFooter />
     </div>
   );
 }
